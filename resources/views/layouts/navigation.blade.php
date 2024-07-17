09b9dc95650813d22702cb8904eba4ef3dc9ea0c
umarhadimukti <umarhadimukti@gmail.com>
2024-07-17 15:52:49 +0700

    fix: 🐛 mobile screen

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,4 +1,4 @@
-<nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
+<nav x-data="{ open: false }" class="bg-white border-b border-gray-100 py-4">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
@@ -13,12 +13,12 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                        <h3 class="text-lg">Dashboard</h3>
                     </x-nav-link>
 
                     @hasanyrole('admin|owner')
                     <x-nav-link :href="route('admin.categories.index')" :active="request()->routeIs('admin.categories.*')">
-                        {{ __('Categories') }}
+                        <h3 class="text-lg">Categories</h3>
                     </x-nav-link>
                     @endhasanyrole
                 </div>
@@ -76,6 +76,12 @@
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
+
+            @hasanyrole('admin|owner')
+            <x-responsive-nav-link :href="route('admin.categories.index')" :active="request()->routeIs('admin.categories.*')">
+                {{ __('Categories') }}
+            </x-responsive-nav-link>
+            @endhasanyrole()
         </div>
 
         <!-- Responsive Settings Options -->
